515ddcd40877f0f4aa76c141572e2c65d6f59fe1
Fl4bergaster <95364745+Fl4bergaster@users.noreply.github.com>
2023-03-24 11:28:04 -0400

    game

--- index.js
+++ index.js
@@ -1,20 +1,69 @@
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
+canvas.width = innerWidth
+canvas.height = innerHeight
+
+const gravity = 0.5
+
 class PLayer {
     constructor(){
         this.position = {
             x: 100,
             y: 100
         }
-        this.width = 100
-        this.height =100
+        this.velocity = {
+            x: 0,
+            y: 1
+        }
+        this.width = 30
+        this.height = 30
     }
 
     draw(){
+        c.fillStyle = 'red'
         c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+
+    update() {
+        this.draw()
+        this.position.y += this.velocity.y
+        
+        if (this.position.y + this.height + this.velocity.y <= canvas.height){
+            this.velocity.y += gravity
+
+        }
+        else this.velocity.y = 0
     }
 }
 
 const player = new PLayer()
-player.draw()
+player.update()
+
+function animate(){
+    requestAnimationFrame(animate)
+    c.clearRect(0,0, canvas.width, canvas.height)
+    player.update()
+}
+animate()
+
+addEventListener('keydown',({ keyCode }) => {
+    console.log(keyCode)
+    switch (keyCode) {
+        case 37:
+            console.log('left')
+            break
+        case 40:
+            console.log('down')
+            break
+        case 39:
+            console.log('right')
+            break
+        case 38:
+            console.log('up')
+            player.velocity.y -= 20
+            break
+
+    }
+})
+
