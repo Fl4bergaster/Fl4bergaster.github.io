5591856fdb9b643ae675a62fd29726695c33120a
Fl4bergaster <95364745+Fl4bergaster@users.noreply.github.com>
2023-03-24 14:19:00 -0400

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ canvas.height = innerHeight
 
 const gravity = 0.5
 
-class PLayer {
+// 
+class Player {
     constructor(){
         this.position = {
             x: 100,
@@ -20,16 +21,19 @@ class PLayer {
         this.height = 30
     }
 
+    // Dessiner notre personnage
     draw(){
         c.fillStyle = 'red'
         c.fillRect(this.position.x, this.position.y, this.width, this.height)
     }
 
+    // Updater la position du personnage
     update() {
         this.draw()
         this.position.x += this.velocity.x
         this.position.y += this.velocity.y
-        
+    
+        // Ajouter de la gravité
         if (this.position.y + this.height + this.velocity.y <= canvas.height){
             this.velocity.y += gravity
 
@@ -38,7 +42,27 @@ class PLayer {
     }
 }
 
-const player = new PLayer()
+class Platform {
+    constructor({x, y}) {
+        this.position = {
+            x,y
+        }
+
+        this.width = 200
+        this.height = 20
+    }
+
+    draw() {
+        c.fillStyle = 'blue'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+}
+
+
+
+const player = new Player()
+const platforms = [new Platform({ x: 200, y: 100 }), new Platform({ x: 500, y: 200 })]
+
 const keys = {
     right:{
         pressed: false
@@ -53,14 +77,43 @@ function animate(){
     requestAnimationFrame(animate)
     c.clearRect(0,0, canvas.width, canvas.height)
     player.update()
+    platforms.forEach(platform => {
+        platform.draw()
+    })
+    
 
-    if (keys.right.pressed) {
+    // player and platform mouvement
+    if (keys.right.pressed && player.position.x < 400) {
         player.velocity.x = 5
     }
-    else if (keys.left.pressed) {
+    else if (keys.left.pressed && player.position.x > 100) {
         player.velocity.x = -5
     }
-    else player.velocity.x = 0
+    else {
+        player.velocity.x = 0
+
+        if (keys.right.pressed) {
+            platforms.forEach(platform => {
+                platform.position.x -= 5
+            })
+        }
+        else if (keys.left.pressed) {
+            platforms.forEach(platform => {
+                platform.position.x += 5
+            })
+        }
+    }
+
+    // platform collison
+    platforms.forEach(platform => {
+    
+        if (player.position.y + player.height <= platform.position.y && 
+            player.position.y + player.height + player.velocity.y >= platform.position.y &&
+            player.position.x + player.width >= platform.position.x &&
+            player.position.x < platform.position.x + platform.width){
+            player.velocity.y = 0
+        }
+    })
     
 }
 animate()
