3545e98fabbbd195828ab054d924bc4427fe6f31
Fl4bergaster <95364745+Fl4bergaster@users.noreply.github.com>
2023-03-30 14:29:56 -0400

    platform modification

--- index.js
+++ index.js
@@ -54,20 +54,33 @@ image.src = './player.png';
 console.log(image)
 
 class Platform {
-    constructor({ x, y}) {
+    constructor({ x, y, text}) {
         this.position = {
             x,y
         }
 
         this.width = 200
         this.height = 20
+        this.text = text
 
     }
 
     draw(color) {
         c.fillStyle = color
         c.fillRect(this.position.x, this.position.y, this.width, this.height)
+        // Draw text inside the platform
+        c.fillStyle = "#000000"; // set text color to black
+        c.font = "12px Arial"; // set font size and type
+        c.textAlign = "center"; // center the text horizontally
+        c.fillText(
+        this.text,
+        this.position.x + this.width / 2,
+        this.position.y + this.height / 2 + 4 // adjust the vertical position
+        )
     }
+
+    
+     
 }
 
 requestAnimationFrame
@@ -76,11 +89,13 @@ const player = new Player()
 const platforms = [
     new Platform({ 
         x: 300, 
-        y: 600}), 
+        y: 600,
+        text: 'new form'}), 
         
         new Platform({ 
         x: 600, 
-        y: 300})
+        y: 300,
+        text: 'rien'})
 ]
 
 const keys = {
@@ -170,7 +185,7 @@ function animate(){
             platform.draw('pink')
             player.isGrounded = true
 
-            if (keys.down.pressed == true){location.href = "next.html";}
+            if (keys.down.pressed == true){location.href = "https://forms.office.com/r/wbZVjvDr9P";}
             
         }
         else {platform.draw('yellow')}
