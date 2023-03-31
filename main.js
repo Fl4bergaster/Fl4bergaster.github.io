810c0c84d921d95b226b6c8501d2293cde3e9770
Fl4bergaster <95364745+Fl4bergaster@users.noreply.github.com>
2023-03-31 11:07:57 -0400

    try

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
+const { Player } = require('./index.js');
 
-import { Player } from 'player.js'
-const player = new Player()
+const player = new Player();
+
 
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
