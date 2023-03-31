0e040c844061936b8aafb75110dda8a019a6bd60
Fl4bergaster <95364745+Fl4bergaster@users.noreply.github.com>
2023-03-31 09:33:44 -0400

    test
    j'essaye de rendre ca clean

---
+++ player.js
@@ -0,0 +1,50 @@
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
+
+canvas.width = innerWidth
+canvas.height = innerHeight
+
+let gravity = 0.5
+
+export class Player {
+    constructor(){
+        this.position = {
+            x: 100,
+            y: 100
+        }
+        this.velocity = {
+            x: 0,
+            y: 0
+        }
+        this.image = image
+        this.width = 64
+        this.height = 64
+        this.isGrounded = true
+        this.jumpMaxHeight = 50
+    }
+
+    // Dessiner notre personnage
+    draw() {
+        c.drawImage(this.image, this.position.x, this.position.y)
+    }
+
+    // Updater la position du personnage
+    update() {
+        this.draw()
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+    
+        // Ajouter de la gravité
+        if (this.position.y + this.height + this.velocity.y <= canvas.height){
+            this.velocity.y += gravity
+
+        }
+        
+        else this.velocity.y = 0
+    }
+
+    
+
+}
+
+
