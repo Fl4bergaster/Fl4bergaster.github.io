1e9c300768ab5b4f08fc47ab4807e11bc36967bf
Fl4bergaster <95364745+Fl4bergaster@users.noreply.github.com>
2023-04-04 13:48:41 -0400

    draw

--- main.js
+++ main.js
@@ -2,6 +2,12 @@ import { Player } from './utils.js'
 
 const player = new Player();
 
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
+
+canvas.width = innerWidth
+canvas.height = innerHeight
+
 
 
 player.draw()
--- utils.js
+++ utils.js
@@ -1,8 +1,22 @@
 export class Player {
-    
+    constructor(){
+        this.position = {
+            x: 100,
+            y: 100
+        }
+        this.velocity = {
+            x: 0,
+            y: 0
+        }
+        this.image = image
+        this.width = 64
+        this.height = 64
+        this.isGrounded = true
+        this.jumpMaxHeight = 50
+    }
     // Dessiner notre personnage
     draw() {
-        console.log('yas')
+        drawImage(this.image, this.position.x, this.position.y)
     }
 
 }
