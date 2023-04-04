68c20927948945cb51da949622eaba8dc1b5db26
Fl4bergaster <95364745+Fl4bergaster@users.noreply.github.com>
2023-04-04 13:22:49 -0400

    Update main.js

--- main.js
+++ main.js
@@ -3,11 +3,7 @@ import { Player } from './utils.js'
 const player = new Player();
 
 
-const canvas = document.querySelector('canvas')
-const c = canvas.getContext('2d')
 
-canvas.width = innerWidth
-canvas.height = innerHeight
 
 
 player.update()
