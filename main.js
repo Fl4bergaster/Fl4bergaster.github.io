abe5ce285d9cd2f46c3a92aeabbd195ad6aadd11
Fl4bergaster <95364745+Fl4bergaster@users.noreply.github.com>
2023-04-05 11:51:33 -0400

    gravity

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { Player } from './utils.js'
+import { Player } from './player.js'
 
 const player = new Player();
 
@@ -9,6 +9,12 @@ const c = canvas.getContext('2d')
 canvas.width = innerWidth
 canvas.height = innerHeight
 
+function animate(){
+    requestAnimationFrame(animate)
+    c.clearRect(0,0, canvas.width, canvas.height)
+    player.draw()
 
-player.draw()
 
+}
+
+animate()
