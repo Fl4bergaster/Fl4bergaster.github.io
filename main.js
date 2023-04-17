21d2915f986b6eb4cb55d88b0bfd26cdc0070bd2
Fl4bergaster <95364745+Fl4bergaster@users.noreply.github.com>
2023-04-17 13:20:10 -0400

    Update main.js

--- main.js
+++ main.js
@@ -58,7 +58,7 @@ function animate(){
     console.log(platformposition)
     if(platformposition > 10){
        // platforms[0].x = platformlenght * 3
-        console.log(platforms[0])
+        console.log(platforms[0].position.x)
 
     }
 
